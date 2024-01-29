0abe04bd12c5799db32b5a3c67da186b11b0d01d
TimWolfskin <timwolfskin013@gmail.com>
2024-01-30 00:05:37 +0300

    first commit

---
+++ backend/bigcorp/celery.py
@@ -0,0 +1,8 @@
+import os
+
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bigcorp.settings")
+app = Celery("bigcorp")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
